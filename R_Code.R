d357dc417e079ef7544e72405bc4e77aa05794bb
millerjoey <arch1190@gmail.com>
2016-04-08 01:37:17 -0400

    Update.
    Added more comments and thoughts. bayesglm() problems.

--- R_Code.R
+++ R_Code.R
@@ -1,5 +1,7 @@
+# Start
 library("dplyr")
 library("ggplot2")
+library("bayesglm")
 
 setwd("Desktop/RTI")
 records <- read.table("records.csv", header = F, sep=",")
@@ -29,6 +31,10 @@ apply(train, 2, function(x) sum(x=="?"))
 # Occupation, workclass, country, have missing values. Will try calling "?" a legitimate class.
 
 #===============First-Order Effects============
+# Only 14 covariates; two of which were collinear.
+# The section below generates plots, considers and
+# justifies transformations.
+
 #-----------------||AGE||----------------------
 # Age, as histogram, colored by mean(over_50k):
 grp <- group_by(train, age)
@@ -41,7 +47,12 @@ ggplot(data=df, aes(x=age, y=count, fill=Y,  color=Y)) +
   scale_x_continuous(breaks=seq(min(train$age),max(train$age), by=5)) +
   labs(title="Histogram of Age shaded by Probability")
 
-# Clearly not increasing (marginal distribution). Will convert to Factor:
+dev.print(pdf, "HistAgeProb.pdf")
+
+# Clearly not increasing (in its marginal distribution). 
+# Will convert to Factor for simplicity. Might want to
+# see if probability is increasing in Age|X for some other
+# covariates.
 
 trainCut <- cbind.data.frame(train, "ageCut"=cut(train$age, breaks = c(16, 23, 27, 35, 55, 62, 67, 95)))
 View(trainCut)
@@ -80,13 +91,16 @@ ggplot(data=df, aes(x=education_num, y=Y)) +
   geom_point() +
   geom_text(label=as.character(df$count), nudge_y = .03) +
   labs(title="P(Y=1) by Education Level (with sample sizes)", x="Education Num/Level")
+# Call View(grp) to see that education_level and education_num are collinear.
+
 
 # Not a large difference between 4-8. Will bin them in a new column and transform:
 educationCut=cut(train$education_num, breaks = c(0.5,1.5,2.5,3.5,8.5,9.5,10.5,11.5,12.5,13.5,14.5,15.5, 16.5))
 train <- cbind.data.frame(trainCut, "educationCut"=as.integer(educationCut))
 
 # Looking again, we have a monotonic relationship and more DOF leftover
-# for interactions:
+# for interactions. It turns out, this also helped to make some trends
+# more consistent for Age|Education level.
 grp <- group_by(train, educationCut)
 df <- summarize(grp, "Y"=mean(over_50k), "count"=n())
 ggplot(data=df, aes(x=educationCut, y=Y)) +
@@ -102,7 +116,8 @@ ggplot(data=df, aes(x=marital_status, y=Y)) +
   theme(axis.text.x = element_text(angle = 45, hjust=1)) +
   geom_text(label=as.character(df$count), nudge_y = .03) +
   labs(title="P(Y=1) by Marital Status (with sample sizes)")
-# TODO: Test interactions by male/female
+# Interactions by male/female turned out to be an important
+# aspect of the model.
 
 #-------------||RACE||--------------
 grp <- group_by(train, race)
@@ -165,7 +180,8 @@ ggplot(data=df, aes(x=hours_week, y=count, fill=Y,  color=Y)) +
   scale_y_log10(breaks=c(2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 4096*2, 4096*4)) +
   scale_color_continuous(name="Prob") +
   labs(title="Histogram of Hours per Week colored by Probability", x="hours per week")
-# Isn't monotonic. Cut arbitrarily into 10 groups.
+# Isn't monotonic. Cut arbitrarily into 10 groups for more
+# sample size, look at interactions.
 dev.print(pdf, "HistHoursProb.pdf")
 
 hoursCut=cut(train$hours_week, breaks = c(0, 10, 20, 30, 40,50,60,70,80,90,100))
@@ -202,7 +218,8 @@ ggplot(data=df, aes(x=country, y=Y)) +
 # Ordered by race x country unhelpfully. Nevertheless, it's clear 
 # that bars are not all ordered identically upon separating by 
 # race. This suggests an interaction.
-# Note: Geom_text is causing trouble with the sorting.
+# Note: Geom_text is causing trouble with the sorting. Will do more
+# detailed interaction plots later.
 
 # I'll add a column for (roughly) continents:
 continents <- function(x) {
@@ -233,6 +250,11 @@ ggplot(data=df, aes(x=continent, y=Y)) +
   labs(title="P(Y=1) by Continent (with sample sizes)")
 
 #==============Second-Order Effects=====================
+
+# This will generate every combination of interaction plots.
+# Two calls to ggplot are because I found the 36 combination
+# via combn() but want to swap x-y axes and make them next to
+# each other in the list.
 trainF <- cbind(train, "educationFactor"= as.factor(train$educationCut))
 list <- combn(names(trainF)[-c(1, 2, 4, 5, 8, 11, 12, 13, 14, 15, 17)], 2, simplify=F)
 for (i in list) {
@@ -257,10 +279,14 @@ for (i in list) {
           labs(title=paste("P(Y=1) in", i[1], "x", i[2])))
 }
 
-# Figure 2. Is generated in the above batch.
 
 # Note: Used new data frame "trainF" to convert education to a factor. Was being
 # evaluated with continuous colors; couldn't change ggplot's default easily.
+
+# Optionally: Add ggplot(data=filter(df, count>30), aes(...)...) 
+# to eliminate the noise or add SE bars. But beware: the lines 
+# will just skip the points and connect, so it may
+# look like more line crossings. SE bars were too messy.
 
 # Discussion and TODO:
 # I generated all pairwise plots to look for interactions. A more thorough
@@ -272,6 +298,10 @@ for (i in list) {
 # Alternatively, would also be interesting to put priors on the interactions 
 # and get the final predictions by sampling from the posterior distribution 
 # of each Beta in the model.
+
+# Important interactions can be recognized by intersections. On this
+# scale, line segment slopes cannot necessarily indicate interactions, but
+# are suggestive.
 
 
 # Below, I looked through the charts generated, made some comments, and started
@@ -313,8 +343,7 @@ for (i in levels(train$occupation)) {
 #----------||HOURSCUT x CONTINENT||---------------
 # Nothing interesting.
 
-#===================Training the Model=========================
-
+#===================Training the Models=========================
 
 fit <- glm(data=train, formula = 
              over_50k ~ 
@@ -322,7 +351,6 @@ fit <- glm(data=train, formula =
              marital_status + occupation +
              educationCut*race + hoursCut + continent*race +
              sex*marital_status + ageCut*occupation
-             #hoursCut*occupation
            ,
            family=binomial(link="logit"))
 
@@ -330,14 +358,31 @@ preds <- predict.glm(object = fit, type = "response", newdata = train)
 
 sum(ifelse(preds>0.5, 1, 0))
 sum(ifelse(preds>0.5, 1, 0)==train$over_50k)/39074
-                           # Accuracy: 85.2% on training data.
+# Accuracy: 85.2% on training data.
+
+# Problem: bayesglm (gets point estimates for the coefs, but allows for a prior)
+# gets radically different, even with noninformative prior. With improper prior,
+# fails to converge. TODO: the math to see that posterior is improper and find
+# better priors, possibly a good idea to get full distributions with MCMC.
+
+# fitb <- bayesglm(data=train, formula =
+#                    over_50k ~
+#                    log(1+capital_gain) + log(1+capital_loss) +
+#                    marital_status + occupation +
+#                    educationCut*race + hoursCut + continent*race +
+#                    sex*marital_status + ageCut*occupation
+#                  , family=binomial(link="logit"), prior.scale = Inf)
+# predsb <- predict.glm(object = fitb, type = "response", newdata = train)
+# sum(ifelse(predsb>0.5, 1, 0))
+# sum(ifelse(predsb>0.5, 1, 0)==train$over_50k)/39074
+
 
 sum(!train$over_50k)/39074 # 'Null"  model. Predicts zero for everyone.
                            # Accuracy: 76.2%
 
 #==================Prediction Function==========================
 
-prediction <- function(newdata) {
+prediction <- function(newdata, model) {
   # Accepts a data frame with names(newdata)="id", "age", "workclass", "education_level", "education_num", 
   #"marital_status", "occupation", "TBD", "race", "sex", 
   #"capital_gain", "capital_loss", "hours_week", "country"
@@ -353,9 +398,9 @@ prediction <- function(newdata) {
   newdata <- cbind.data.frame(newdata, hoursCut, ageCut, educationCut, continent)
   
   # Use 0-1 loss. Returns vector of 1s and 0s rounded.
-  preds <- predict.glm(object = fit, type = "response", newdata)
+  preds <- predict.glm(object = model, type = "response", newdata)
   return(ifelse(preds>0.5, 1, 0))
 }
 
-sum(prediction(test)==test$over_50k)/9768
+sum(prediction(test, fit)==test$over_50k)/9768
                 # Final Accuracy: 84.3%
